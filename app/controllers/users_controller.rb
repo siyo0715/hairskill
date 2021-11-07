7add253c26259594fb4ce970f4b2e0f52b76ad48
siyo0715 <hms0175km@gmail.com>
2021-11-07 18:19:48 +0000

    ten commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:top, :about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def index
+    @user = current_user
   end
+end
+
+private
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :phone_number)
 end
