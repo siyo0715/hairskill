2a9e620122ad1ca2435cadb3c1025361fea2ca99
siyo0715 <hms0175km@gmail.com>
2021-11-09 13:29:11 +0000

    i

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,15 +90,15 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    enable_starttls_auto => true,
-    address => "smtp.gmail.com",
-    port => 587,
-    domain => 'smtp.gmail.com',
-    user_name => "SMTP_USERNAME",
-    password => "SMTP_PASSWORD",
-    authentication => 'login'
+      port: 587,
+      address: 'smtp.gmail.com',
+      domain: 'smtp.gmail.com',
+      user_name: ENV['SMTP_USERNAME'],
+      password: ENV['SMTP_PASSWORD'],
+      enable_starttls_auto: true
   }
 end
